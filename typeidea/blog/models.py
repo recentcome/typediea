5ddde13a4b292820d428a3d363a68f306b595193
wwp <recentcome@163.com>
2021-07-26 22:50:35 +0800

    第6章 6.1节admin模块更新

--- typeidea/blog/models.py
+++ typeidea/blog/models.py
@@ -1,7 +1,6 @@
 from django.contrib.auth.models import User
 from django.db import models
 
-
 # Create your models here.
 from django.db.models import CASCADE
 
@@ -13,6 +12,9 @@ class Category(models.Model):
         (STATUS_NORMAL, '正常'),
         (STATUS_DELETE, '删除'),
     )
+
+    def __str__(self):
+        return self.name
 
     name = models.CharField(max_length=50, verbose_name='名称')
     status = models.PositiveIntegerField(default=STATUS_NORMAL, choices=STATUS_ITEMS, verbose_name="状态")
@@ -31,6 +33,10 @@ class Tag(models.Model):
         (STATUS_NORMAL, '正常'),
         (STATUS_DELETE, '删除'),
     )
+
+    def __str__(self):
+        return self.name
+
     name = models.CharField(max_length=50, verbose_name='名称')
     status = models.PositiveIntegerField(default=STATUS_NORMAL, choices=STATUS_ITEMS, verbose_name="状态")
     owner = models.ForeignKey(User, verbose_name="作者", on_delete=CASCADE)
@@ -50,6 +56,9 @@ class Post(models.Model):
         (STATUS_DRAFT, '草稿'),
     )
 
+    def __str__(self):
+        return self.name
+
     title = models.CharField(max_length=255, verbose_name="标题")
     desc = models.CharField(max_length=1024, blank=True, verbose_name="摘要")
     content = models.TextField(verbose_name="正文", help_text="正文必须为MarkDown格式")
@@ -57,6 +66,7 @@ class Post(models.Model):
     owner = models.ForeignKey(User, verbose_name="作者", on_delete=CASCADE)
     created_time = models.DateTimeField(auto_now_add=True, verbose_name="创建时间")
     category = models.ForeignKey(Category, verbose_name="分类", on_delete=CASCADE)
+    tag = models.ManyToManyField(Tag, verbose_name="标签")
 
     class Meta:
         verbose_name = verbose_name_plural = "文章"
--- typeidea/config/models.py
+++ typeidea/config/models.py
@@ -1,7 +1,6 @@
 from django.contrib.auth.models import User
 from django.db import models
 
-
 # Create your models here.
 from django.db.models import CASCADE
 
@@ -41,12 +40,10 @@ class SideBar(models.Model):
     title = models.CharField(max_length=50, verbose_name="标题")
     content = models.CharField(max_length=500, blank=True,
                                verbose_name='内容', help_text="如果设置的不是HTML类型，可为空")
+    display_type = models.PositiveIntegerField(default=1, choices=SIDE_TYPE, verbose_name="展示类型")
     status = models.PositiveIntegerField(default=STATUS_SHOW, choices=STATUS_ITEMS, verbose_name="状态")
     owner = models.ForeignKey(User, verbose_name="作者", on_delete=CASCADE)
     created_time = models.DateTimeField(auto_now_add=True, verbose_name="创建时间")
 
     class Meta:
         verbose_name = verbose_name_plural = "侧边栏"
-
-
-
